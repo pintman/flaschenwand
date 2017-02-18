4cdfe37e7933121905a1e36b5ff9c7442a414ab7
Marco Bakera <bakera@tbs1.de>
2017-02-18 17:41:23 +0100

    bugfix: removed values for width and height, using default values.

--- src/demo_plasma.py
+++ src/demo_plasma.py
@@ -10,24 +10,20 @@ import flaschenwand
 import time
 import math
 
-# Width and height of the display
-WIDTH = flaschenwand.DEFAULT_WIDTH
-HEIGHT = flaschenwand.DEFAULT_HEIGHT
-
 
 def main():
-    fw = flaschenwand.Flaschenwand(WIDTH, HEIGHT)
+    fw = flaschenwand.Flaschenwand()
 
     while True:
         current = time.time()
 
-        for x in range(WIDTH):
-            for y in range(HEIGHT):
+        for x in range(fw.width):
+            for y in range(fw.height):
                 v = math.sin(x*10.0 + current)
                 # -1 < sin() < +1
                 # therfore correct the value and bring into range [0, 1]
                 v = (v+1.0) / 2.0
-                fw.set_pixel_rgb(x, y, 0, 0, 0, white=int(v*255))
+                fw.set_pixel_rgb(x, y, int(v*255), 0, 0)
 
         fw.show()
     
