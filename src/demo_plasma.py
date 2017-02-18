a8dd9e4761f049d5981fa3f971990c07cd95aaef
Marco Bakera <bakera@tbs1.de>
2017-02-18 17:59:09 +0100

    color cycling

--- src/demo_plasma.py
+++ src/demo_plasma.py
@@ -9,7 +9,7 @@ Implementation details can be found here: http://www.bidouille.org/prog/plasma
 import flaschenwand
 import time
 import math
-
+import colorsys
 
 def main():
     fw = flaschenwand.Flaschenwand()
@@ -19,11 +19,13 @@ def main():
 
         for x in range(fw.width):
             for y in range(fw.height):
-                v = math.sin(x*10.0 + current)
+                v = math.sin(x*25.0 + current)
                 # -1 < sin() < +1
                 # therfore correct the value and bring into range [0, 1]
                 v = (v+1.0) / 2.0
-                fw.set_pixel_rgb(x, y, int(v*255), 0, 0)
+                # transform from hsv to rgb: cycle through colors.
+                r, g, b = colorsys.hsv_to_rgb(v, 1, v)
+                fw.set_pixel_rgb(x, y, int(r*255), int(g*255), int(b*255))
 
         fw.show()
     
