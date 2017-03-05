41ac8d299bbb6356543d6e1510d5d9b728456d39
Marco Bakera <marco@bakera.de>
2017-03-05 20:31:59 +0000

    text now scrollable.

--- src/abc_scroller.py
+++ src/abc_scroller.py
@@ -1,18 +1,33 @@
 import flaschenwand
 import time
 
+class DisplayItem:
+    def __init__(self, char, pos):
+        self.char = char
+        self.pos = pos
+
 fw = flaschenwand.Flaschenwand()
 fnt = flaschenwand.Font()
 
-# Each character is 3 pixels width
-chars = "abcdefghijklmnopqrstuvwxyz"
+# Each character is 3 pixels width + 1 pixel space
+chars = "hallo welt "
+
+# create dictionary to hold character and x-position to be displayed.
+chars_pos = []
+pos = 4
+for c in chars:
+    chars_pos += [DisplayItem(c, pos)]
+    pos += 4
 
 fw.set_all_pixels_rgb(0,0,0)
 
-for c in chars:
-    for x in range(fw.width):
-        fw.set_all_pixels_rgb(0,0,0)
-        fnt.set_char(fw, c, x)
-        fw.show()
-        time.sleep(0.2)
-
+for i in range(len(chars_pos)*4):
+    fw.set_all_pixels_rgb(0,0,0)    
+    for item in chars_pos:
+        fnt.set_char(fw, item.char, item.pos)
+        item.pos -= 1
+        
+    fw.show()
+    
+    time.sleep(0.2)
+    
