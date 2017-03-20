392034870f402d55f43a141e9a02de095cbee0f9
Marco Bakera <marco@bakera.de>
2017-03-20 16:34:04 +0000

    workload now in threaded worker, prep for frequency.

--- src/oscserver.py
+++ src/oscserver.py
@@ -7,24 +7,29 @@ import flaschenwand
 import pythonosc.dispatcher
 import pythonosc.osc_server
 import os
+import threading
+import time
+import math
+
 
 class OSCServer:
     def __init__(self, ip="0.0.0.0", port=5555):
+        # mapping noted to colors
         self.note_color = { 0:"red", 1:"green", 2:"blue"}
-        self.fw = flaschenwand.Flaschenwand()
-        # thre color values [r,g,b]
-        self.colors = [127,127,127]
 
         disp = pythonosc.dispatcher.Dispatcher()
         disp.map("/noteon/0/", self._handle_colors_rgb)
+        disp.map("/noteon/1/", self._handle_freq)
         disp.map("/noteon/9/", self._handle_shutdown)
 
         server = pythonosc.osc_server.ThreadingOSCUDPServer((ip, port), disp)
 
         print("Serving on {}".format(server.server_address))
         fnt = flaschenwand.Font()
-        fnt.scroll_text(self.fw, "osc ready")
+        fnt.scroll_text(self.fw, "osc")
 
+        self.worker = FlaschenwandWorker()
+        self.worker.start()
         server.serve_forever()
 
     def _handle_colors_rgb(self, _msg, note, val):
@@ -33,18 +38,77 @@ class OSCServer:
         """
         if note in self.note_color:
             # val in [0,128], therefore take the double
-            self.colors[note] = 2 * val
-            self.fw.set_all_pixels_rgb(*self.colors)
-            self.fw.show()
+            color = self.note_color[note]
+            self.worker.colors[color] = 2 * val
 
+    def _handle_freq(self, msg, note, val):
+        if note == 0:
+            self.worker.freq = val
+            
     def _handle_shutdown(self, _msg, _note, _val):
         #print("shutdown received.", note, val)
-        fnt = flaschenwand.Font()
-        fnt.scroll_text(self.fw, "bye")
-
+        self.worker.scroll("bye")
         os.system("shutdown -h now")
 
+
+class FlaschenwandWorker(threading.Thread):
+    def __init__(self):
+        super().__init__()
+        self.pause = False
+        self.fw = flaschenwand.Flaschenwand()
+        self.colors = {"red":127, "green":127, "blue":127}
+        self.freq = 4
+
+    def run(self):
+        print("worker started")
+        current = 0
+        while True:
+            if self.pause:
+                continue
+            current += 0.1
+            #current = time.time()
+            for x in range(self.fw.width):
+                for y in range(self.fw.height):
+                    r,g,b = self._rgb_at(x,y, current)                    
+                    self.fw.set_pixel_rgb(x, y, r,g,b)
+
+            self.fw.show()            
+            print(self.colors, "freq", self.freq)
+            time.sleep(0.1)
+
+    def _rgb_at(self, x, y, clock_time):
+        #v = math.sin(2.0 * math.pi * self.freq * x + clock_time)
+        v = math.sin(2*math.pi*self.freq*x + clock_time)
+        #v = math.sin(2*3*self.freq*x + clock_time)
+        # -1 < sin() < +1, correct value, bring into range [0, 1]
+        v = (v+1.0) / 2.0
+        v=1 # TODO remove
+        r = self.colors["red"]/256 * v
+        g = self.colors["green"]/256 * v
+        b = self.colors["blue"]/256 * v
+        return int(r*255), int(g*255), int(b*255)
+
+    def scroll(self, text):
+        self.pause = True
+        fnt = flaschenwand.Font()
+        fnt.scroll_text(self.fw, text)
+        self.pause = False
             
+
+"""
+class Signal(threading.Thread):
+    def __init__(self, min_val, max_val):
+        self.range = (min_val, max_val)
+    def run(self):
+        while True:
+            
+        pass
+    def __call__(self):
+        return None
+
+class Color(Signal):
+    def __init__(self, 
+"""            
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--ip",
