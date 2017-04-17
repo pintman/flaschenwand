6c681b062c2ed064b376a1712fa37311e8c80aa5
Marco Bakera <marco@bakera.de>
2017-04-17 11:37:45 +0000

    init version of test program.

---
+++ src/test_flaschenwand.py
@@ -0,0 +1,26 @@
+"""
+Some tests for correct connection
+"""
+
+import flaschenwand
+import time
+import math
+import colorsys
+
+def main():
+    fw = flaschenwand.Flaschenwand()
+
+    while True:
+        current = time.time()
+
+        for r,g,b in [(1,1,1), (1,0,0), (0,1,0), (0,0,1)]:
+            for x in range(fw.width):
+                for y in range(fw.height):
+                    fw.set_all_pixels_rgb(0,0,0)
+                    fw.set_pixel_rgb(x, y, int(r*255), int(g*255), int(b*255))
+                    fw.show()
+                    time.sleep(0.5)
+    
+
+if __name__ == "__main__":
+    main()
